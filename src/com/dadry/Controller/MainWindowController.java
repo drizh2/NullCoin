b89c25ac9f13024cd4abd86e088773ed340f6696
drizh2 <drizh2@ukr.net>
2025-07-24 13:43:10 +0300

    Added javafx-fxml library. Added Base64 util java class. Created javafx scenes. Created controllers for javafx scenes.

---
+++ src/com/dadry/Controller/MainWindowController.java
@@ -0,0 +1,89 @@
+package com.dadry.Controller;
+
+import com.dadry.Model.Transaction;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
+import java.util.Base64;
+import java.util.Optional;
+import java.util.logging.Logger;
+
+public class MainWindowController {
+    Logger logger = Logger.getLogger(getClass().getName());
+
+    @FXML
+    public TableView<Transaction> tableview = new TableView<>();
+    @FXML
+    private TableColumn<Transaction, String> from;
+    @FXML
+    private TableColumn<Transaction, String> to;
+    @FXML
+    private TableColumn<Transaction, Integer> value;
+    @FXML
+    private TableColumn<Transaction, String> timestamp;
+    @FXML
+    private TableColumn<Transaction, String> signature;
+    @FXML
+    private BorderPane borderPane;
+    @FXML
+    private TextField nullCoins;
+    @FXML
+    private TextArea publicKey;
+
+    public void initialize() {
+        Base64.Encoder encoder = Base64.getEncoder();
+        from.setCellValueFactory(
+                new PropertyValueFactory<>("fromFX"));
+        to.setCellValueFactory(
+                new PropertyValueFactory<>("toFX"));
+        value.setCellValueFactory(
+                new PropertyValueFactory<>("value"));
+        signature.setCellValueFactory(
+                new PropertyValueFactory<>("signature"));
+        timestamp.setCellValueFactory(
+                new PropertyValueFactory<>("timestamp"));
+        nullCoins.setText(BlockchainData.getInstance().getWalletBalanceFX());
+        publicKey.setText(encoder.encodeToString(WalletData.getInstance().getWallet().getPublicKey().getEncoded()));
+        tableview.setItems(BlockchainData.getInstance().getTransactionLedgerFX());
+        tableview.getSelectionModel().select(0);
+    }
+
+    @FXML
+    public void toNewTransactionController() {
+        Dialog<ButtonType> newTransactionController = new Dialog<>();
+        newTransactionController.initOwner(borderPane.getScene().getWindow());
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("../View/AddNewTransactionWindow.fxml"));
+        try {
+            newTransactionController.getDialogPane().setContent(fxmlLoader.load());
+        } catch (IOException e) {
+            logger.info("Cant load dialog");
+            e.printStackTrace();
+            return;
+        }
+        newTransactionController.getDialogPane().getButtonTypes().add(ButtonType.FINISH);
+        Optional<ButtonType> result = newTransactionController.showAndWait();
+        if (result.isPresent() ) {
+            tableview.setItems(BlockchainData.getInstance().getTransactionLedgerFX());
+            nullCoins.setText(BlockchainData.getInstance().getWalletBallanceFX());
+        }
+    }
+
+    @FXML
+    public void refresh() {
+        tableview.setItems(BlockchainData.getInstance().getTransactionLedgerFX());
+        tableview.getSelectionModel().select(0);
+        nullCoins.setText(BlockchainData.getInstance().getWalletBallanceFX());
+    }
+
+    @FXML
+    public void handleExit() {
+        BlockchainData.getInstance().setExit(true);
+        Platform.exit();
+    }
+}
